ecb396e23556ec48bf9f80a46d1750a6aba24761
Almady, Balazs <balazs.almady@gmail.com>
2022-04-25 20:12:58 +0200

    feat(stats): Statistics view added

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import FeedbackList from "./components/FeedbackList.svelte";
+import FeedbackStats from "./components/FeedbackStats.svelte";
 	import { IFeedback, INIT_FEEDBACKS } from "./models/feedbacks";
 
 	let feedbacks: Array<IFeedback> = INIT_FEEDBACKS;
+
+	$: count = feedbacks.length
+	$: average = feedbacks.reduce((avg: number, {rating} ) => avg + rating, 0) / feedbacks.length
 
 	const deleteFeedback = (e: CustomEvent) => {
 		const itemId: number = e.detail;
@@ -11,6 +15,7 @@
 </script>
 
 <main class="container">
+	<FeedbackStats {count} {average}/>
 	<FeedbackList {feedbacks} on:delete-feedback={deleteFeedback} />
 </main>
 
