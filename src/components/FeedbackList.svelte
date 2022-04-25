92bb111583034e447e232707e698604daefbe91a
Almady, Balazs <balazs.almady@gmail.com>
2022-04-25 19:42:51 +0200

    init(list): scaffolded list component and models

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { IFeedback } from "../models/feedbacks";
+
+    export let feedbacks: Array<IFeedback> = [];
+</script>
+
+{#each feedbacks as fb (fb.id)}
+    <h3>{fb.text}</h3>
+    <br>
+{/each}
